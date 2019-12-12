7e82351bf0898830ec06acc847bad6ec42ef0eac
kokolem <31248031+kokolem@users.noreply.github.com>
2019-12-13 00:23:38 +0100

    add bookmarkmanager

---
+++ bookmarkmanager/precache-manifest.5167bc4a03e5e87a4c885c8c030d8f51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aac3366e6a8277ebab9304d01269c85",
+    "url": "/bookmarkmanager/index.html"
+  },
+  {
+    "revision": "a3b5ba4b05aa94e3ac95",
+    "url": "/bookmarkmanager/static/js/2.1bc180e1.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/bookmarkmanager/static/js/2.1bc180e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3af53a88558f36dc18da",
+    "url": "/bookmarkmanager/static/js/main.a39d8e8e.chunk.js"
+  },
+  {
+    "revision": "d6d8e8f69db2b0a62b15",
+    "url": "/bookmarkmanager/static/js/runtime-main.ec737cd2.js"
+  }
+]);
---
+++ bookmarkmanager/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox bookmarkmanager configuration
+ * and re-run your bookmarkmanager process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bookmarkmanager/precache-manifest.5167bc4a03e5e87a4c885c8c030d8f51.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookmarkmanager/index.html"), {
+
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
